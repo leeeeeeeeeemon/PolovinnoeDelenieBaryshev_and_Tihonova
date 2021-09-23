9cc0865bf6eadc4e70e4c6eca52ddd32832d69ff
lemonjuice <secondemilbarish@gmail.com>
2021-09-23 17:26:05 +0300

    Добавьте файлы проекта.

---
+++ PolovinnoeDelenieBaryshev_and_Tihonova/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 1;
+        double b = 2;
+        double e = 0.001;
+
+        while ((b - a) > 2 * e)
+        {
+            double c = (a + b) / 2;
+
+            if (Function(a) * Function(c) < 0)
+            {
+                b = c;
+
+            }
+            else
+            {
+                a = c;
+            }
+        }
+        double x = (a + b) / 2;
+        Console.WriteLine(Math.Round(x, 3));
+    }
+
+    public static double Function(double x)
+    {
+
+        return x* Math.Sin(x) - 1;
+    }
+
+    
+}
